65a02d9a6a006955ed6b68099fa5a8e475a5457b
jonpena <gran_200014@hotmailcom>
2023-03-13 00:32:05 -0400

    deleting dist folder to branch main

--- dist/utils/error.handler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleHttp = void 0;
-const handleHttp = (customError, res, error) => {
-    console.log(error);
-    res.status(400);
-    res.send({ customError, error });
-};
-exports.handleHttp = handleHttp;
